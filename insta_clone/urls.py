fdcd52c5432ce0a121525092e8951ea209b217fa
memm1991 <72989632+memm1991@users.noreply.github.com>
2021-08-09 11:07:02 -0300

    templates y archivos estaticos

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from users import views as users_views
 
 
 
@@ -17,6 +18,8 @@ urlpatterns = [
     path('query/',local_views.query),
     path('params/<str:name>/<int:age>',local_views.params),
     #posts path
-    path('posts/', posts_views.list_posts)
+    path('posts/', posts_views.list_posts),
+    #users path
+    path('users/login', users_views),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
