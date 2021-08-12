24d4c96ae5d146057d11b86ff165700083ccd717
memm1991 <72989632+memm1991@users.noreply.github.com>
2021-08-12 20:34:05 -0300

    update and refactoring on templates and control of form for profile update

--- users/views.py
+++ users/views.py
@@ -5,15 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-# Exception
-from django.db.utils import IntegrityError
-
-# Models
-from django.contrib.auth.models import User
-from users.models import Profile
-
 # Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 def update_profile(request):
@@ -64,30 +57,22 @@ def login_view(request):
 
 def signup(request):
     """Sign up view."""
-    if request.method == 'POST':
-        username = request.POST['username']
-        passwd = request.POST['passwd']
-        passwd_confirmation = request.POST['passwd_confirmation']
+    if request.method=='POST':
+        form=SignupForm(request.Post)
+        if form.is_valid():
+         form.save()
+         return redirect('login')
+    else:
+        form= SignupForm()
+    return render(
+        request=request,
+        template_name='user/signup.html',
+        context={'form':form}
+    )
+     
+     
+     
 
-        if passwd != passwd_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-
-        try:
-            user = User.objects.create_user(username=username, password=passwd)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
-
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email']
-        user.save()
-
-        profile = Profile(user=user)
-        profile.save()
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
 
 
 @login_required
